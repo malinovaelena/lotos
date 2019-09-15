bdfe565171d045809b1f17bf3af6dde6bada393b
Елена <malinovael@yandex.ru>
2019-09-16 00:20:10 +0300

    start project

---
+++ Desktop/cmder/lotos/lotos/src/components/main-background/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'main.css';
+
+export default class Main extends React.Component {
+    render() {
+        return(<div className="main">
+            
+        </div>)
+    }
+};
